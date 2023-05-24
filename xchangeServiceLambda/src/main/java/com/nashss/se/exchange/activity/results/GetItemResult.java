335ecf1107cfc93960e9a7bb2218239d215a3135
Hillary <hillarymartin.nss@gmail.com>
2023-05-24 08:52:40 -0700

    GetItemResult created.

--- xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/results/GetItemResult.java
+++ xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/results/GetItemResult.java
@@ -1,4 +1,41 @@
 package com.nashss.se.exchange.activity.results;
 
+import com.nashss.se.exchange.Models.ItemModel;
+
 public class GetItemResult {
+    private final ItemModel item;
+
+    private GetItemResult(ItemModel item) {
+        this.item = item;
+    }
+
+    public ItemModel getItem() {
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        return "GetItemResult{" +
+                "item=" + item +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private ItemModel item;
+
+        public Builder withPlaylist(ItemModel item){
+            this.item = item;
+            return this;
+        }
+
+        public GetItemResult build() {
+            return new GetItemResult(item);
+        }
+    }
+
 }
