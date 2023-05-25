9a754ffd862ee6249b9dec6303fd08a84d57e6ef
Hillary <hillarymartin.nss@gmail.com>
2023-05-25 11:44:18 -0700

    Added a GSI to .yaml. builds with sam and gradle.

--- xchangeServiceLambda/src/main/java/com/nashss/se/exchange/dynamodb/ItemDao.java
+++ xchangeServiceLambda/src/main/java/com/nashss/se/exchange/dynamodb/ItemDao.java
@@ -1,15 +1,18 @@
 package com.nashss.se.exchange.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.ScanResultPage;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static com.nashss.se.exchange.dynamodb.Item.ZIPCODE_TYPE_INDEX;
 
 //import static com.nashss.se.exchange.dynamodb.models.Item.ZIPCODE_TYPE_INDEX;
 
@@ -32,19 +35,6 @@ public class ItemDao {
             throw new IllegalArgumentException("Could not find item");
         }
         return item;
-    }
-
-    public List<Item> getItems(Item previousEndItem) {
-        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-                .withLimit(15);
-
-        if(previousEndItem != null) {
-            Map<String, AttributeValue> startKeyMap = new HashMap<>();
-            startKeyMap.put("itemId", new AttributeValue().withS(previousEndItem.getItemId()));
-            scanExpression.setExclusiveStartKey(startKeyMap);
-        }
-        ScanResultPage<Item> orderPage = mapper.scanPage(Item.class, scanExpression);
-        return orderPage.getResults();
     }
 
 
@@ -84,24 +74,24 @@ public class ItemDao {
      * @param type type to search for RangeKey
      * @return list of items that match the zipCode and type
      */
-//    public List<Item> searchItems(String zipCode, String type) {
-//        Map<String, AttributeValue> valueMap = new HashMap<>();
-//        valueMap.put(":type", new AttributeValue().withS(type));
-//        valueMap.put(":zip_Code", new AttributeValue().withS(zipCode));
-//        DynamoDBQueryExpression<Item> queryExpression = new DynamoDBQueryExpression<Item>()
-//                .withIndexName(ZIPCODE_TYPE_INDEX)
-//                .withConsistentRead(false)
-//                .withKeyConditionExpression("type = :type and zipCode = :zip_Code")
-////                .withLimit()
-//                .withExpressionAttributeValues(valueMap);
-//
-//        //searchResults will only have items that have zip and type, no other information.
-//        PaginatedQueryList<Item> searchResults = mapper.query(Item.class, queryExpression);
-//        //fullyLoadedInfo gets all attributes of each item in searchResults
-//        List<Item> fullyLoadedInfo = new ArrayList<>();
-//        for(Item item : searchResults) {
-//            fullyLoadedInfo.add(mapper.load(item));
-//        }
-//        return fullyLoadedInfo;
-//    }
+    public List<Item> searchItems(String zipCode, String type) {
+        Map<String, AttributeValue> valueMap = new HashMap<>();
+        valueMap.put(":type", new AttributeValue().withS(type));
+        valueMap.put(":zip_Code", new AttributeValue().withS(zipCode));
+        DynamoDBQueryExpression<Item> queryExpression = new DynamoDBQueryExpression<Item>()
+                .withIndexName(ZIPCODE_TYPE_INDEX)
+                .withConsistentRead(false)
+                .withKeyConditionExpression("type = :type and zipCode = :zip_Code")
+//                .withLimit()
+                .withExpressionAttributeValues(valueMap);
+
+        //searchResults will only have items that have zip and type, no other information.
+        PaginatedQueryList<Item> searchResults = mapper.query(Item.class, queryExpression);
+        //fullyLoadedInfo gets all attributes of each item in searchResults
+        List<Item> fullyLoadedInfo = new ArrayList<>();
+        for(Item item : searchResults) {
+            fullyLoadedInfo.add(mapper.load(item));
+        }
+        return fullyLoadedInfo;
+    }
 }
