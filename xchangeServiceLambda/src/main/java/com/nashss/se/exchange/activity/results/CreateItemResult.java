d6a26b2b18efb3943a52adf28a85cfb5b72bbca3
Hillary <hillarymartin.nss@gmail.com>
2023-06-01 09:51:59 -0700

    CreateItemResult created.

--- xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/results/CreateItemResult.java
+++ xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/results/CreateItemResult.java
@@ -1,7 +1,42 @@
 package com.nashss.se.exchange.activity.results;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.nashss.se.exchange.Models.ItemModel;
 
 
 public class CreateItemResult {
+    private final ItemModel itemModel;
+
+    private CreateItemResult(ItemModel itemModel) {
+        this.itemModel = itemModel;
+    }
+
+    public ItemModel getItem() {
+        return itemModel;
+    }
+
+
+    @Override
+    public String toString() {
+        return "CreateItemResult{" +
+                "itemModel=" + itemModel +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private ItemModel itemModel;
+
+        public Builder withPlaylist(ItemModel itemModel) {
+            this.itemModel = itemModel;
+            return this;
+        }
+
+        public CreateItemResult build() {
+            return new CreateItemResult(itemModel);
+        }
+    }
 }
