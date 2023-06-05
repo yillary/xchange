bf99522e554b869a218fa5dfeedf9b70a422ef15
Hillary <hillarymartin.nss@gmail.com>
2023-06-05 13:03:04 -0700

    added a member variable to CreateMemberRequest

--- xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/requests/CreateMemberRequest.java
+++ xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/requests/CreateMemberRequest.java
@@ -5,21 +5,20 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 @JsonDeserialize(builder = CreateMemberRequest.class)
 public class CreateMemberRequest {
     private final String zipCode;
+    private final String memberId;
 
 
-    public CreateMemberRequest(String zipCode) {
+    public CreateMemberRequest(String zipCode, String memberId) {
         this.zipCode = zipCode;
+        this.memberId = memberId;
     }
 
     public String getZipCode() {
         return zipCode;
     }
 
-    @Override
-    public String toString() {
-        return "CreateMemberRequest{" +
-                "zipCode='" + zipCode + '\'' +
-                '}';
+    public String getMemberId() {
+        return memberId;
     }
 
     //CHECKSTYLE:OFF:Builder
@@ -27,16 +26,29 @@ public class CreateMemberRequest {
         return new CreateMemberRequest.Builder();
     }
 
+    @Override
+    public String toString() {
+        return "CreateMemberRequest{" +
+                "zipCode='" + zipCode + '\'' +
+                ", memberId='" + memberId + '\'' +
+                '}';
+    }
+
     public static class Builder {
         private String zipCode;
+        private String memberId;
 
         public CreateMemberRequest.Builder withZipCode(String zipCode) {
             this.zipCode = zipCode;
             return this;
         }
 
+        public CreateMemberRequest.Builder withMemberId(String memberId) {
+            this.memberId = memberId;
+            return this;
+        }
         public CreateMemberRequest build() {
-            return new CreateMemberRequest(zipCode);
+            return new CreateMemberRequest(zipCode, memberId);
         }
     }
 }
