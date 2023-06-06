6b3155d00b1b53928284bfe6f93c3656786c464b
Hillary <hillarymartin.nss@gmail.com>
2023-06-06 07:43:25 -0700

    GetMemberItemResult created

--- xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/results/GetMemberItemsResult.java
+++ xchangeServiceLambda/src/main/java/com/nashss/se/exchange/activity/results/GetMemberItemsResult.java
@@ -1,42 +1,39 @@
-//package com.nashss.se.exchange.activity.results;
-//
-//import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-//import com.nashss.se.exchange.activity.requests.GetItemRequest;
-//import com.nashss.se.exchange.activity.requests.GetMemberItemsRequest;
-//
-//@JsonDeserialize(builder = GetMemberItemsRequest.class)
-//public class GetMemberItemsResult {
-//    private final String memberId;
-//
-//    public GetMemberItemsResult(String memberId) {
-//        this.memberId = memberId;
-//    }
-//
-//    public String getMemberId() {
-//        return memberId;
-//    }
-//
-//
-//    @Override
-//    public String toString() {
-//        return "GetMemberItemsResult{" +
-//                "memberId='" + memberId + '\'' +
-//                '}';
-//    }
-//
-//    //CHECKSTYLE:OFF:Builder
-//    public static Builder builder() {
-//        return new Builder();
-//    }
-//
-//    public static class Builder {
-//        private String memberId;
-//
-//        public Builder withMemberId(String memberId) {
-//            this.memberId = memberId;
-//            return this;
-//        }
-//
-//        public GetMemberItemsRequest build() { return new GetMemberItemsResult(memberId); }
-//    }
-//}
+package com.nashss.se.exchange.activity.results;
+
+import com.nashss.se.exchange.dynamodb.Item;
+
+import java.util.Set;
+
+
+public class GetMemberItemsResult {
+    private final Set<Item> memberItems;
+
+    public GetMemberItemsResult(Set<Item> memberItems) {
+        this.memberItems = memberItems;
+    }
+
+    public Set<Item> getMemberItems() {
+        return memberItems;
+    }
+
+    @Override
+    public String toString() {
+        return "GetMemberItemsResult{" +
+                "memberItems=" + memberItems +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() { return new Builder(); }
+
+    public static class Builder {
+        private Set<Item> memberItems;
+
+        public Builder withMemberItems(Set<Item> items) {
+            this.memberItems = items;
+            return this;
+        }
+
+        public GetMemberItemsResult build() { return new GetMemberItemsResult(memberItems); }
+    }
+}
